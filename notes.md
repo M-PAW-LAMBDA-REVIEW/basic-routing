c0fd18d7fdc6aa77ed284fcea70b7f92b11298f4
Michael Walter <michael_patrick.walter@outlook.com>
2021-01-09 22:07:09 -0800

    Playing with history push().

---
+++ notes.md
@@ -0,0 +1,4 @@
+# Steps
+
+1. install: react-router-dom
+2. import { BrowserRoute, Route, Link} from the above
--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import {Route, Link} from 'react-router-dom';
 import './App.css';
+
+const Home = (props) => {
+  return <h1>Home Component</h1>
+};
+
+/**
+ * 
+ * @param {*} props 
+ * @function push
+ * Description:
+ *  Push a route onto the history stack.
+ */
+const About = (props) => {
+  const {push} = props.history;
+  return <>
+    <h1>About Component</h1>
+    <button onClick={() => push('/')}>Back</button>
+    </>
+};
+
+const Contact = (props) => {
+  return <h1>Contact Component</h1>
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+
       </header>
     </div>
   );
